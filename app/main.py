092e378bb1a1862cd9791aa1722d669d78838a7d
Ajay Prem Shankar <ajaypremshankar@gmail.com>
2024-10-05 12:52:28 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 import sys
 import os
-
+import zlib
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    # print("Logs from your program will appear here!")
 
     # Uncomment this block to pass the first stage
     
@@ -16,9 +16,22 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+    elif command == 'cat-file':
+        if sys.argv[2] == "-p":
+            hash = sys.argv[3]
+            dir = hash[:2]
+            file_name = hash[2:]
+            raw = read_file(f".git/objects/{dir}/{file_name}")
+            header, content = raw.split(b"\0", maxsplit=1)
+            print(content.decode("utf-8"), end="", sep="")
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
+
+def read_file(file_path: str):
+    with open(file_path, "rb") as file:
+        content = zlib.decompress(file.read())
+    return content
 
 if __name__ == "__main__":
     main()
